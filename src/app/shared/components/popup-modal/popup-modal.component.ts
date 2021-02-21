300b4e76d29360ba72700ff40ed78ff2c806816d
anmgupta <63639605+anmgupta@users.noreply.github.com>
2021-02-21 18:04:15 +0530

    Upload

---
+++ src/app/shared/components/popup-modal/popup-modal.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-popup-modal',
+  templateUrl: './popup-modal.component.html',
+  styleUrls: ['./popup-modal.component.scss']
+})
+export class PopupModalComponent implements OnInit {
+
+  constructor(public dialogRef: MatDialogRef<PopupModalComponent>) { }
+
+  ngOnInit() {
+  }
+
+  // When the user clicks the action button a.k.a. the logout button in the\
+  // modal, show an alert and followed by the closing of the modal
+  actionFunction() {
+    alert("You have logged out.");
+    this.closeModal();
+  }
+
+  // If the user clicks the cancel button a.k.a. the go back button, then\
+  // just close the modal
+  closeModal() {
+    this.dialogRef.close();
+  }
+}
---
+++ src/app/shared/constants/app-constants.ts
@@ -0,0 +1,5 @@
+
+export class AppConstants {
+  public static userName = "blogger@grapecity.com";
+  public static password = "1qaz!QAZ";
+}
